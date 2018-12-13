b4534852ed58f7898f9a853a41d895a647c9eee8
Wojciech Pismenko <wojciechpismenko@gmail.com>
2018-12-13 17:05:25 +0100

    added 2nd quest

--- array.js
+++ array.js
@@ -3,3 +3,14 @@ const arr = ['doniczka', 'szklanka', 'zeszyt', 'telefon', 'torba'];
 console.log(arr);
 console.log(arr.length);
 
+for (let i = 0; i < arr.length; i+=1) {
+    console.log(arr[i] + ' ' + arr[i].length);
+};
+
+for (let el of arr) {
+    console.log(el + ' ' + el.length);
+};
+
+arr.forEach(function(el) {
+    console.log(el + ' ' + el.length);
+});
