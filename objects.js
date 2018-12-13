2ba5044918ef96a87ad8b77e7efc51f836329e5c
Wojciech Pismenko <wojciechpismenko@gmail.com>
2018-12-13 20:13:31 +0100

    added 2nd quest

--- objects.js
+++ objects.js
@@ -16,3 +16,20 @@ console.log("Produkt numer jeden to: " + prod1.name);
 console.log("Produkt numer dwa to: " + prod2.name);
 console.log("Produkty kosztują razem: " + (prod1.price + prod2.price));
 console.log("Produkty ważą razem: " + (prod1.weight + prod2.weight));
+
+const currentUser = {
+    name : "Wojciech",
+    surname : "Pismenko",
+    email : "wojciechpismenko@gmail.com",
+    www : "pismenko.pl",
+    userType : "editor",
+    show : function() {
+        console.log("Imię: ", this.name);
+        console.log("Nazwisko: ", this.surname);
+        console.log("Email: ", this.email);
+        console.log("Strona www: ", this.www);
+        console.log("Typ użytkownika: ", this.userType);
+    }
+};
+
+currentUser.show();
