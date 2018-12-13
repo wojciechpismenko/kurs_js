c5f1f9664dd08280c92f342543dadf7090deae78
Wojciech Pismenko <wojciechpismenko@gmail.com>
2018-12-13 20:50:48 +0100

    added 4th quest

--- objects.js
+++ objects.js
@@ -51,3 +51,39 @@ const book = {
 }
 
 book.showDetails();
+
+const users = [
+    //id name surname email age money
+    [  1, "Shauna", "Bradnocke", "sbradnocke0@altervista.org", 20, 108.28 ],
+    [  2, "Mela", "Redman", "mredman1@nps.gov", 24, 267.37 ],
+    [  3, "Othelia", "Lemon", "olemon2@slashdot.org", 15, 748.06 ],
+    [  4, "Meier", "Cockell", "mcockell3@icio.us", 32, 1951.64 ],
+    [  5, "Shellysheldon", "Gronowe", "sgronowe4@cnbc.com", 16, 1040.54 ],
+    [  6, "Francisca", "Tofanini", "ftofanini5@gnu.org", 21, 1544.08 ],
+    [  7, "Cliff", "Underwood", "cunderwood6@addtoany.com", 10, 451.21 ],
+    [  8, "Caron", "Falshaw", "cfalshaw7@hugedomains.com", 27, 1968.72 ],
+    [  9, "Anitra", "Warters", "awarters8@intel.com", 12, 380.68 ],
+    [ 10, "Caitrin", "Baudrey", "cbaudrey9@ihg.com", 13, 1385.44 ]
+]
+
+function fixData(tab) {
+    const newTab = [];
+
+    for (const el of tab) {
+        const obj = {
+            id : el[0],
+            name : el[1],
+            surname : el[2],
+            email : el[3],
+            age : el[4],
+            money : el[5]
+        }
+        newTab.push(obj);
+    }
+
+    return newTab;
+}
+
+console.log(users);
+console.log(fixData(users));
+
